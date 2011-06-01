688c1bf476b22b4145e4f4adb8af637c62d8d0dc
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2011-06-01 23:55:37 +0200

    Initial commit
    Adds a button that when clicked loads jQuery from googles CDN by adding a script
    element to body in the current tab.

---
+++ click.js
@@ -0,0 +1,3 @@
+var s = document.createElement('script');
+s.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js');
+document.getElementsByTagName('body')[0].appendChild(s);
